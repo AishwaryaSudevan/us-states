21e82555e27053800c345948a7caa654f84f6e20
John Leszczynski <jrleszczynski@gmail.com>
2016-05-17 06:45:16 -0400

    color states blue

--- js/main.js
+++ js/main.js
@@ -2,21 +2,33 @@
 // var $ = require('jquery')
 var d3 = require('d3')
 
-var path = d3.geo.path()
 
 var width = 960,
-    height = 1160;
+    height = 960;
 
 var svg = d3.select('body').append('svg')
   .attr('width', width)
   .attr('height', height);
 
-d3.json('../data/us-states.json', function (json) {
-  console.log(json)
+var projection = d3.geo.albersUsa()
+  .translate([width / 2, height / 2])
+  .scale([1000]);
+
+var path = d3.geo.path()
+  .projection(projection)
+
+
+d3.json('../data/us-states.json', function (error, json) {
+  if (error) {
+    console.log("error getting us-states.json: ", error)
+    return
+  }
   svg.selectAll('path')
     .data(json.features)
     .enter()
     .append('path')
     .attr('d', path)
+    .style('fill', 'steelblue')
+
 
 })
