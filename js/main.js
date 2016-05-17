b2e263e794422a2301c0513c92d7f56d4b7485b4
John Leszczynski <jrleszczynski@gmail.com>
2016-05-17 06:01:07 -0400

    load states json and display

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 /* Uncomment the following line to use jQuery */
 // var $ = require('jquery')
+var d3 = require('d3')
 
-console.log('Hello world, from main.js!')
+var path = d3.geo.path()
+
+var width = 960,
+    height = 1160;
+
+var svg = d3.select('body').append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+d3.json('../data/us-states.json', function (json) {
+  console.log(json)
+  svg.selectAll('path')
+    .data(json.features)
+    .enter()
+    .append('path')
+    .attr('d', path)
+
+})
